f606cbc9599d73f9c2c0cf197fd0b4716047096e
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-17 01:15:08 +0600

    setup the basic things

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('project is running')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`project is running on port ${PORT}`);
+})
