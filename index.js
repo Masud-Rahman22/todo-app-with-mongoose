0cc1f50dfb0522695610acd3908f23301c91f4da
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-17 01:19:16 +0600

    start working with mongoose and added a error handler

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+function errorHandler(err,res,res,next){
+    if(res.headersSent){
+        return next(err);
+    }
+    res.status(500).json({error : err})
+}
 
 app.get('/', (req,res)=>{
     res.send('project is running')
