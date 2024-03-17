a322548ca36598f205a6af18007e9e89c00d0cfe
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-17 12:41:35 +0600

    created schema for a todo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const todoHandler = require('./routeHandler/todoHandler')
 
 app.use(express.json());
--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const todoSchema = require('../schemas/todoSchema');
 const router = express.Router();
+const Todo = new mongoose.model('Todo', todoSchema)
 
 // all todos
 router.get('/', async(req,res)=>{
@@ -13,7 +16,18 @@ router.get('/:id', async(req,res)=>{
 
 // create one todo
 router.post('/', async(req,res)=>{
-
+    const newTodo = new Todo(req.body);
+    await newTodo.save()
+        .then(() => {
+            res.status(200).json({
+                message: "Todo was inserted successfully"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "There was a server side error"
+            });
+        });
 })
 
 // create more than one todo
