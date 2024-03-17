8c94d32fb0e0e7381a5e4244349ff0bed2c49b21
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-17 11:18:16 +0600

    all routes are created

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const todoHandler = require('./routeHandler/todoHandler')
 
 app.use(express.json());
 
-function errorHandler(err,res,res,next){
-    if(res.headersSent){
-        return next(err);
-    }
-    res.status(500).json({error : err})
-}
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aw2xu1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(()=>console.log('Connected to MongoDB'))
+    .catch((err)=>console.log('could not connect to MongoDB',err))
+
+// function errorHandler(err,res,res,next){
+//     if(res.headersSent){
+//         return next(err);
+//     }
+//     res.status(500).json({error : err})
+// }
+
+app.use('/todo', todoHandler)
 
 app.get('/', (req,res)=>{
     res.send('project is running')
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// all todos
+router.get('/', async(req,res)=>{
+    
+})
+
+// one particular todo
+router.get('/:id', async(req,res)=>{
+
+})
+
+// create one todo
+router.post('/', async(req,res)=>{
+
+})
+
+// create more than one todo
+router.post('/all', async(req,res)=>{
+
+})
+
+// update a todo
+router.put('/:id', async(req,res)=>{
+
+})
+
+// delete one todo
+router.delete('/:id', async(req,res)=>{
+
+})
+
+module.exports = router;
